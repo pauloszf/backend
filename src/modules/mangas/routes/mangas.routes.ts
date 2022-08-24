cc65593bb78916de6c3b6c9e7749fdaac1ce3ddf
Pauloszf <paulosergio.zaguini@gmail.com>
2022-08-24 10:00:43 -0400

    Adicionado middleware celebrate

--- src/modules/mangas/routes/mangas.routes.ts
+++ src/modules/mangas/routes/mangas.routes.ts
@@ -1,14 +1,55 @@
 import { Router } from 'express';
 import MangasController from '../controllers/MangasController';
-import ListMangaService from '../typeorm/services/ListMangaService';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const mangasRouter = Router();
 const mangasController = new MangasController();
 
 mangasRouter.get('/', mangasController.index);
-mangasRouter.get('/:id', mangasController.show);
-mangasRouter.post('/', mangasController.create);
-mangasRouter.put('/:id', mangasController.update);
-mangasRouter.delete('/:id', mangasController.delete);
+
+mangasRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  mangasController.show
+);
+
+mangasRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      mangaName: Joi.string().required(),
+      cap: Joi.number().precision(1).required(),
+    },
+  }),
+  mangasController.create
+);
+
+mangasRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      mangaName: Joi.string().required(),
+      cap: Joi.number().precision(1).required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  mangasController.update
+);
+
+mangasRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  mangasController.delete
+);
 
 export default mangasRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,18 +1,22 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors'
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import { conectarServidorNoBD } from '@config/db';
 
 const app = express();
+conectarServidorNoBD();
 
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
-conectarServidorNoBD();
+app.use(errors());
+
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
