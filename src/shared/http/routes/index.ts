d4e27b79e1d7bde9b67dc54da4c8f2e26e54f76a
Pauloszf <paulosergio.zaguini@gmail.com>
2022-08-29 06:56:59 -0400

    Modulo de gerenciamento de perfil de usuario

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import mangasRouter from '@modules/mangas/routes/mangas.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 
 const routes = Router();
 
@@ -10,6 +11,7 @@ routes.use('/mangas', mangasRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 
 
